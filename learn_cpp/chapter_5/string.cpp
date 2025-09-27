33dc6cc42c204589337018f85d1d4301eadebb50
Arman Jasuja <arman_jasuja@yahoo.com>
2025-09-27 15:54:26 +0400

    Basic usage of std::string

---
+++ learn_cpp/chapter_5/string.cpp
@@ -0,0 +1,11 @@
+// Getting started with std::string
+#include <string>
+#include <iostream>
+
+
+int main(void)
+{
+    std::string name{ "Arman" };
+    std::cout << name << '\n';
+    return 0;
+}
